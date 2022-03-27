0976d9d6586bfbc6564ecee079321d2e1d3844de
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-03-27 23:28:04 +0530

    change color code and remove unused code

--- components/indexwithcolor.jsx
+++ components/indexwithcolor.jsx
@@ -5,7 +5,7 @@ function IndexWithColor() {
   return (
     <>
       {aql_level.map((data, key) => (
-        <div className="m-5 " key={key}>
+        <div className="m-2 " key={key}>
           <button
             className={`${data.className} w-[106px] p-2 text-black font-bold rounded-sm`}
           >
