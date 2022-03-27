6f76c5c6ecf811c68908b8f84f0f6fbc02f326ed
Rajeshwar Kashyap <work.rjkashyap05@gmail.com>
2022-03-27 18:31:08 +0530

    show City Details

---
+++ components/indexwithcolor.js
@@ -0,0 +1,20 @@
+import React from "react";
+import aql_level from "../constents/aqllevel";
+
+function IndexWithColor() {
+  return (
+    <>
+      {aql_level.map((data, key) => (
+        <div className="m-5 ">
+          <button
+            className={`${data.className} w-[106px] p-2 text-black font-bold rounded-sm`}
+          >
+            {data.name}
+          </button>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default IndexWithColor;
